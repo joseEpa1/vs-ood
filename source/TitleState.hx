30a0101578c499e96639873c5e25ff892c5dcc81
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-05-19 09:01:51 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -23,7 +23,9 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
+#if newgrounds 
 import io.newgrounds.NG;
+#end
 import lime.app.Application;
 import openfl.Assets;
 
@@ -53,14 +55,18 @@ class TitleState extends MusicBeatState
 
 	override public function create():Void
 	{
+		#if android
+		FlxG.android.preventDefaultKeys = [BACK];
+		#end
+			
 		#if polymod
-		polymod.Polymod.init({modRoot: "mods", dirs: ['introMod']});
+	//	polymod.Polymod.init({modRoot: "mods", dirs: ['introMod']});
 		#end
 		
-		#if sys
+		/*#if sys
 		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
 			sys.FileSystem.createDirectory(Sys.getCwd() + "/assets/replays");
-		#end
+		#end*/
 
 		@:privateAccess
 		{
@@ -284,7 +290,7 @@ class TitleState extends MusicBeatState
 		#end
 
 		if (pressedEnter && !transitioning && skippedIntro)
-		{
+		{       #if newgrounds
 			#if !switch
 			NGio.unlockMedal(60960);
 
@@ -292,7 +298,7 @@ class TitleState extends MusicBeatState
 			if (Date.now().getDay() == 5)
 				NGio.unlockMedal(61034);
 			#end
-
+                        #end
 			if (FlxG.save.data.flashing)
 				titleText.animation.play('press');
 
